7071c98df7c5bb43fd2ea29313d2db1384054293
prajwal <prajwalprajun1997@gmail.com>
2022-06-01 13:07:20 +0530

    2nd commit spicejet

--- src/test/java/Com/spicejetTest/SpiceJetBookingTest.java
+++ src/test/java/Com/spicejetTest/SpiceJetBookingTest.java
@@ -13,7 +13,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SpiceJetBookingTest {
 	
   public static void main(String[] args) throws InterruptedException {
-	  
+	 //open the browser
+ 
 	  WebDriverManager.chromedriver().setup();
 	  ChromeOptions opt=new ChromeOptions();
 	  opt.addArguments("--disable-notifications");
