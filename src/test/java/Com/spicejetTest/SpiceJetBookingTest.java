2e7a3ec9b33c6aae8a31c312c2de3b882117d57f
prajwal <prajwalprajun1997@gmail.com>
2022-06-01 15:57:18 +0530

    2nd commit

--- src/test/java/Com/spicejetTest/SpiceJetBookingTest.java
+++ src/test/java/Com/spicejetTest/SpiceJetBookingTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SpiceJetBookingTest {
-	
+	//open the browser
   public static void main(String[] args) throws InterruptedException {
 	  
 	  WebDriverManager.chromedriver().setup();
